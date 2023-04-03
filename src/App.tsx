297f431a2185c39c46d6741c7a2fb0f6da9d0db0
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-03 22:45:54 +0530

    content update

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import React, { useState } from "react";
 
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 //this app type is react functional component
 const App: React.FC = () => {
-
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
 
   return (
     <div className="App">
-      <span className="heading">
-        Taskify
-        <InputField todo={todo} setTodo={ setTodo} />
-      </span>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList /> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../components/styles.css";
 
 //we can use interface to gave this types
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
 //rafce - short key to react functional component
@@ -15,19 +16,26 @@ interface Props {
 //method -02
 //const InputField:React.FC<Props> = ({ todo, setTodo }: Props) => {
 
-/*this one is basically not that much necessary  because we know 
+/*this one is basically not that much necessary  because we know
 that is functional component */
-const InputField: React.FC<Props> = ({ todo, setTodo }: Props) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-          <input
-              type="input"
-              value={todo}
-              onChange={
-                  (e)=>setTodo(e.target.value)
-              }
-              placeholder="Enter a task"
-              className="input_box" />
+    /* if we want to fire the button on clicking button we can
+      call handleAdd function on forms onSubmit phase */
+      <form className="input" onSubmit={(e) => { 
+          handleAdd(e)
+          inputRef.current?.blur()
+      }}>
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input_box"
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
