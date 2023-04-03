141cb7138875e2902101510026deea03f3cff5f8
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-03 22:16:07 +0530

    setting up inputfield component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import InputField from "./components/InputField";
 
 //this app type is react functional component
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className="App">
       <span className="heading">
         Taskify
-        <InputField />
+        <InputField todo={todo} setTodo={ setTodo} />
       </span>
     </div>
   );
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import "../components/styles.css";
 
+//we can use interface to gave this types
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
 //rafce - short key to react functional component
-const InputField = () => {
+
+//method - 01
+// const InputField = ({ todo, setTodo }: Props) => {
+
+//method -02
+//const InputField:React.FC<Props> = ({ todo, setTodo }: Props) => {
+
+/*this one is basically not that much necessary  because we know 
+that is functional component */
+const InputField: React.FC<Props> = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input_box" />
-      <button className="input_submit" type="submit">Go</button>
+          <input
+              type="input"
+              value={todo}
+              onChange={
+                  (e)=>setTodo(e.target.value)
+              }
+              placeholder="Enter a task"
+              className="input_box" />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
     </form>
   );
 };
 
 export default InputField;
-
